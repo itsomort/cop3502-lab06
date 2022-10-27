e625e54a52db62c3ea66ac0f1819bec9a05c6ad1
omar <omarsal890@gmail.com>
2022-10-27 17:13:36 -0400

    added subtraction functionality

--- adder.py
+++ adder.py
@@ -1,6 +1,17 @@
 def add(a, b):
     return a + b
 
-num1 = int(input("first number to add: "))
-num2 = int(input("second number to add: "))
-print(f"{num1} + {num2} = {add(num1, num2)}")
+def sub(a, b):
+    return a - b
+
+num1 = int(input("first number: "))
+num2 = int(input("second number: "))
+
+op = int(input("input 1 to add and 2 to subtract: "))
+
+if op == 1:
+    print(f"{num1} + {num2} = {add(num1, num2)}")
+elif op == 2:
+    print(f"{num1} - {num2} = {sub(num1, num2)}")
+else: 
+    print("wrong number :(")
